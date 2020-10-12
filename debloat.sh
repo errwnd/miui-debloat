5810f05e23b76ddcfde9568a3df2180430df3f02
neodarg <70091991+neodarg@users.noreply.github.com>
2020-10-12 21:38:29 +0530

    changed for loop

--- debloat.sh
+++ debloat.sh
@@ -4,6 +4,14 @@ com.miui.msaglobal
 com.miui.msa.global
 com.miui.clodservice
 )
-for i in "${list[@]}";do
-  adb shell pm uninstall --user 0 $i
- done
+
+for %%x in (
+"com.miui.mipicks"
+"com.miui.msaglobal"
+"com.miui.msa.global"
+"com.miui.clodservice"
+"bg.projectoria.appinspector") do (
+	adb shell pm uninstall --user 0 %%x
+)
+echo done
+
